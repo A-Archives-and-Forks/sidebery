e8c0a619dbaa28fbc71b13209b2b005309672976
mbnuqw <maxbadryzlov@gmail.com>
2025-12-30 21:29:02 +0500

    chore: refactoring: less logs

--- src/services/storage.fg.ts
+++ src/services/storage.fg.ts
@@ -9,12 +9,12 @@ export * from 'src/services/storage'
 let storageBuf: Stored = {}
 let storageBufTimeout: number | undefined
 async function _set(newValues: Stored): Promise<void> {
-  Logs.info('Storage.fg._set:', Object.keys(newValues))
+  // Logs.info('Storage.fg._set:', Object.keys(newValues))
   const srcInfo = { type: Info.instanceType, winId: Windows.id, tabId: Info.currentTabId }
   return IPC.bg('saveInLocalStorage', newValues, srcInfo)
 }
 export async function set(newValues: Stored, delay?: number): Promise<void> {
-  Logs.info('Storage.fg.set:', Object.keys(newValues))
+  // Logs.info('Storage.fg.set:', Object.keys(newValues))
   if (!delay) return _set(newValues)
 
   storageBuf = { ...storageBuf, ...newValues }
